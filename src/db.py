e0dc37d0bd6be038cbebed473b0a7b95d20e377c
aigulkhkmv <aigul03.14@gmail.com>
2021-06-13 21:50:50 +0300

    FIX: cell run

--- src/db.py
+++ src/db.py
@@ -3,18 +3,29 @@ from time import time
 import psycopg2
 
 
-class DataBase:
-    def __init__(self, database_name):
+class SearchTime:
+    def __init__(self, database_name: str):
         self.database_name = database_name
         conn = psycopg2.connect(database=self.database_name)
         self.curs = conn.cursor()
 
-    def subs_search_first_in(self, mol_smi):
+    def subs_search_first_in(self, mol_smi: str):
         self.curs.execute("select * from rdk.mols where m@>%s", (mol_smi,))
         start_time = time()
         self.curs.fetchone()
         end_time = time()
         return end_time - start_time
 
-    def similarity_search(self):
-        pass
+    def similarity_search(self, fp_type: str, mol_smi: str):
+        #TODO add morganbv_fp function usage
+        if fp_type == "mfp2":
+            # self.curs.callproc("morganbv_fp", (mol_smi,))
+            # fp_for_mol = self.curs.fetchone()[0]
+            # print(fp_for_mol)
+            fp_for_mol = self.curs.execute("CALL morganbv_fp(%s);", (mol_smi,))
+            # fp_for_mol = self.curs.execute("SELECT morganbv_fp(%s)", (mol_smi,))
+            self.curs.execute("select * from rdk.fps where mfp2%s", fp_for_mol)
+            start_time = time()
+            self.curs.fetchone()
+            end_time = time()
+            return end_time - start_time
