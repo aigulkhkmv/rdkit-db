da4730a3d24967a1c3df4d2415e86442a3a8e308
aigulkhkmv <aigul03.14@gmail.com>
2021-06-13 19:17:47 +0300

    NEW: info about time, notebooks with tests

--- src/db.py
+++ src/db.py
@@ -1,17 +1,20 @@
-import psycopg2
 from time import time
+
+import psycopg2
 
 
 class DataBase:
     def __init__(self, database_name):
         self.database_name = database_name
         conn = psycopg2.connect(database=self.database_name)
-        curs = conn.cursor()
-        self.curs = curs
+        self.curs = conn.cursor()
 
-    def subs_search_first_in(self):
-        self.curs.execute("select * from rdk.mols where m@>%s", ("c1ccccc1O",))
+    def subs_search_first_in(self, mol_smi):
+        self.curs.execute("select * from rdk.mols where m@>%s", (mol_smi,))
         start_time = time()
-        self.curs.fetcone()
+        self.curs.fetchone()
         end_time = time()
         return end_time - start_time
+
+    def similarity_search(self):
+        pass
